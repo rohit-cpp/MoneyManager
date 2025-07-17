99392d0257c0484ea76d1b9cb1f1671f50b77b36
Rohit <rrgawanderohit@gmail.com>
2025-07-17 13:11:43 +0530

    project setup is completed

---
+++ src/main/java/com/example/EPQLProject/EpqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EPQLProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpqlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpqlProjectApplication.class, args);
+	}
+
+}
